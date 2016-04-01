ed7e4435fb567058ab5ed2ef34b11634f090d986
Archer.Yu <s0962593@mail.ncyu.edu.tw>
2016-04-01 11:12:06 +0800

    add some annotation

--- Log4J-Example-Thread/src/ThreadLogger.java
+++ Log4J-Example-Thread/src/ThreadLogger.java
@@ -4,6 +4,10 @@ import org.apache.logging.log4j.Logger;
 /**
  * @author Archer.Yu
  * @since 20160328
+ * <p>--------------------------------------------------------------------------------------
+ * <p>a Logger with the name "test-log4j2-threads" is obtained from the LogManager. 
+ * <p>Next, the logger is used to write the thread message in main, 
+ * <p>however the message will be written only if the Logger is configured to allow informational messages.
  */
 
 public class ThreadLogger {
